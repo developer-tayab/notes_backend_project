131cf3538eee4da5d078b200ce1c8f96f5e93718
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-01-06 10:26:27 +0500

    SetUp server on localhost 3000 and some add code essential

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req,res )=>{
+res.send("hello world");
+})
+
+const Port = 3000;
+app.listen(Port,()=>{
+  console.log(`Server is running at http://localhost:${Port}`)
+})
